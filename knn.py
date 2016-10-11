67fb78073b77145e696e0e5770c222e2e95610b3
Alex Khovansky <alx.khovansky@gmail.com>
2016-10-12 02:53:35 +0300

    Recall, precision, accuracy, f1, temporary disabled some output

--- knn.py
+++ knn.py
@@ -80,17 +80,43 @@ polar_data = np.float32(polar_data)
 pol_plot = plt.subplot(111, projection='polar')
 pol_plot.scatter(polar_data[:, 0], polar_data[:, 1], color = color_data)
 pol_plot.grid(True)
-plt.show()
+# plt.show()
 #get k-cross validation
 train_data, test_data = kFold(data, 10)
 #print(test_data, train_data)
 #test = np.delete(data, range(0, 4), axis = 0)
 #print(len(test))
 
-print(test_data[0][0], test_data[0][1])
-print(euDist(test_data[0][0], test_data[0][1]))
+# print(test_data[0][0], test_data[0][1])
+# print(euDist(test_data[0][0], test_data[0][1]))
 
-print(classify(train_data[0], test_data[0], 10, 2))
+classified_data = classify(train_data[0], test_data[0], 10, 2)
+
+# Float so that automatic integer division doesn't fuck up the results
+tp = tn = fp = fn = 0.0
+
+for i in range(len(classified_data)):
+    # if test_data is 1 (1 is truthy here)
+    if test_data[0][i][2]:
+        # t_d is 1, so if c_d is 1 too - we have TP, else - FN
+        if classified_data[i] == test_data[0][i][2]: tp += 1
+        else: fn += 1
+    else:
+        # same deal but TN and FP now
+        if classified_data[i] == test_data[0][i][2]: tn += 1
+        else: fp += 1
+
+# print(tp, tn, fp, fn)
+
+positive_ex = tp + fn
+negative_ex = fp + tn
+recall = tp / positive_ex
+precision = tp / (tp + fp)
+
+accuracy = (tp + tn) / (positive_ex + negative_ex)
+f1 = 2 * (precision * recall) / (precision + recall)
+
+print(recall, precision, accuracy, f1)
 
 tmp = [int(i[2]) for i in test_data[0]]
-print(tmp)
+# print(tmp)
